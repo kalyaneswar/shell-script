6731ffa7ebca65e54b89e80ddf9525112fc9d093
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-22 13:00:41 +0530

    vatiable2.0 script commit

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# declaring variable
+PERSON1=$1
+PERSON2=$2
+
+# refering variable
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I'm fine what about you?"
+echo "$PERSON1: I'm also fine, how's the work?"
+echo "$PERSON2: It's good I'm upgrading to DevOps"
