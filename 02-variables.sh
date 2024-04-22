979d3a5079add12fa7da4f46343166053e1cc026
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-22 12:22:01 +0530

    vatiable script commit

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hi Ramesh, How are you?"
+echo "Ramesh: Hi Suresh, I'm fine what about you?"
+echo "Suresh: I'm also fine, how's the work?"
+echo "Ramesh: It's good I'm upgrading to DevOps"
