9da18d72375f6f06f57d188a13948e580e628280
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-22 12:42:18 +0530

    vatiable2.0 script commit

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I'm fine what about you?"
+echo "$PERSON1: I'm also fine, how's the work?"
+echo "$PERSON2: It's good I'm upgrading to DevOps"
