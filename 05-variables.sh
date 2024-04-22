3dd8d660922bd1e16d0efcd2bb67decd626799b7
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-22 13:10:59 +0530

    vatiable2.0 script commit

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter username:"
+read USERNAME #enter username
+
+echo "enter passwd:"
+read PASSWORD #enter passwd{-s hilde the input from appearing in screen}
+
+echo "your username is $USERNAME, your passwd is $PASSWORD "
