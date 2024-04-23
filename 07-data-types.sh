c8456544210f43c9f81d199b7d01dbc22b50a92e
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-23 12:05:12 +0530

    array script commit

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# one way
 num1=$1
 num2=$2
 
-sum=$(($num1+$num2))
-echo "Total no of $num1 and $num2 is :$sum"
+sum=$($num1+$num2)
+echo "Total no of sum $num1 and $num2 is :$sum"
+
+# another way 
+
+
+# Add the numbers using expr command
+sum_b=$(expr $num1+$num2)
+
+# Print the result
+echo "The sum of $num1 and $num2 is: $sum_b"
