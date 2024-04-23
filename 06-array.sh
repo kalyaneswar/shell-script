704d10e23e915303ed004a96143a09d9a66615e2
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-23 11:48:17 +0530

    array script commit

--- 06-array.sh
+++ 06-array.sh
@@ -7,3 +7,6 @@ MOVIES=("AWE" "ONE" "DJ")
 echo "First movie is ${MOVIES[0]}"
 echo "Second movie is ${MOVIES[1]}"
 echo "Third movie is ${MOVIES[2]}"
+
+# to get all list
+echo "Third movie is ${MOVIES[@]}"
