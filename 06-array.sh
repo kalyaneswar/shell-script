5ee9fd4aa90626f2d42353af7bdaeb2aa2857ef4
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-04-23 11:40:10 +0530

    array script commit

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=["AWE" "ONE" "DJ"]
+
+# arrays as ways starts with 0
+
+echo "First movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[1]}"
+echo "Third movie is ${MOVIES[2]}"
