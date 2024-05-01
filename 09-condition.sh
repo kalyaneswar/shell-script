082f2ad4c9edbe2c0631eca9ff72315653425f91
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-05-01 08:02:27 +0530

    array script commit

--- 09-condition.sh
+++ 09-condition.sh
@@ -6,6 +6,11 @@ NUM=$1
 if [ $NUM -gt 10 ]
 then
     echo "Number is grater than 10"
+elif [$NUM -eq 10]
+then
+    echo "Number is equal to 10."
 else 
     echo "Number is less than 10"
 fi
+
+# expressions for arthematic -gt, lt, -eq, -ge, -le
