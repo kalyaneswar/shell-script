6cec0a4a3db29518fb688bb531e621be53f1ccc6
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-05-02 08:23:35 +0530

    array script commit

---
+++ 12.Colours.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=date +%F:%H:%M:%S
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "Script started executing"
+
+VALIDATE(){
+    # echo "Exist status: $1"
+    # echo "What are you doing : $2"
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2..$G SUCESS $N"
+    fi
+}
+
+# check user is super user ot not(root)
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root access."
+    exit 1 #manually exist if error comes.
+else
+    echo "You are root user."
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y
+VALIDATE $? "Installing Git"
+
+dnf install dockerr -y
+VALIDATE $? "Installing Docker"
