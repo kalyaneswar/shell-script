119972cb5d89278efa905460a30d22ac45f58822
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-05-04 12:35:49 +0530

    new final

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,7 @@ COURSE="Devops Practice from current script"
 echo "Before Calling other script , course: $COURSE"
 echo "Process id of current shell script: $$"
 
-# ./16-others-script.sh
+./16-others-script.sh
 
 echo "Before Calling other script , course: $COURSE"
 
