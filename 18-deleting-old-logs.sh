45da3f626ad06d645c80d2f738f34f49a2b72f25
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-05-04 16:06:45 +0530

    new final

--- 18-deleting-old-logs.sh
+++ 18-deleting-old-logs.sh
@@ -12,3 +12,10 @@ else
     echo -e "$R Source Direcotry doesn't exist ,$N Please make sure direcotory $G $SOURCE_DIRECTORY $N exist "
     exit 1
 fi
+
+# to crate old dates files
+# touch -d 20240401 kalyan{1..5}.txt
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.txt" -mtime +14)
+
+echo "files to delete $FILES"
